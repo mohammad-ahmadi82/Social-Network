bce0570bcc31f352c819cb4bc850e57944737f26
shayan <mohammadahmadi.2082@gmail.com>
2023-05-22 22:04:57 +0330

    video 8 specific clean

--- Social/account/views.py
+++ Social/account/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts   import render
-from django.views       import View
-from .forms             import UserRegistrationForm
-
-
+from django.shortcuts           import render, redirect
+from django.views               import View
+from .forms                     import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib             import messages
 
 class RegisterView(View):
+    form_class  = UserRegistrationForm
+    template_name   = 'account/register.html'
     def get(self, request):
-        form = UserRegistrationForm()
-        return render(request, 'account/register.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        return render(request, 'account/register.html')
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'User created successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
+
 
 
